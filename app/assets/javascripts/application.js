af08a05f16d37fa54ae6e46215468673f967a735
Ronak Prajapati <ronak0110@gmail.com>
2013-12-13 04:17:36 +0530

    Devise Configured

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,26 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+ $(document).ready(function () {  
+  var top = $('#navigationbar1').offset().top - parseFloat($('#navigationbar1').css('marginTop').replace(/auto/, 100));
+  
+  $(window).scroll(function (event) {
+  	
+    // what the y position of the scroll is
+    var y = $(this).scrollTop();
+
+    // whether that's below the form
+    if (y >= top) {
+      // if so, ad the fixed class
+      $('#navigationbar1').addClass('navbar-fixed-top');
+    } else {
+      // otherwise remove it
+      $('#navigationbar1').removeClass('navbar-fixed-top');
+    }
+  });
+ });
+
