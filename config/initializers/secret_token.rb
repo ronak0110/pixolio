1471898d3280ec16e66a469605dc2f341c63b25c
Ronak Prajapati <ronak0110@gmail.com>
2013-12-13 01:16:19 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pixolio::Application.config.secret_key_base = 'd3b6bf44818fc4a2552b9414f59e18b7fb747e603f051f5b6f785a64e6baeeb14e9cd8bc8c60962e58827cda5b1a63466b3939d5c83b18c351e40ea869f96efa'
